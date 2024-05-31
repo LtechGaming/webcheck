4f2f9c0b2d9805ded0a7a3912b3c064988530039
Ltech <143030637+LtechGaming@users.noreply.github.com>
2024-05-31 09:12:41 +0100

    Update 1.1.0(info update)
    Added changelog.txt, expanded character limit, added info, stopped crashes, and updated readme.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-This is a WIP project.
+This is a WIP project built in python for checking websites to see if they are legit. Be aware that this may not always be accurate, and may flag legit sites, or not flag scam sites.
+
+License: MIT license
+
+Download: http://ltit.mcandrew.me.uk/projects.html or just use this github page
--- Webcheck.py
+++ Webcheck.py
@@ -1,6 +1,9 @@
 #Web checker python 3.12 edition
-ac = 'qwertyuipopasdfghjklzxcvbnm:/#%1234567890."! '
+print("Starting p1")
+ac = 'qwertyuipopasdfghjklzxcvbnm:/#%1234567890."! @_()-'
 import urllib.request
+print("Finish")
+print("Webcheck v1.1.0 in python")
 while True:
     w = input("Website to check (read instructions): ")
     ic = []
@@ -18,13 +21,20 @@ while True:
     icr = "succeeded"
     print("Character check " + str(icr) + " with a result of " + str(ics) + " and " + str(ica) + " bad characters")
     if ica > 0:
-        print("The bad characters are: " + str(ic) + ". Please read the instructions for more info")
+        print("The bad characters are: " + str(ic) + ".")
+        i = input("Press i for more info, otherwise press anything else: ")
+        if i == "i":
+            print("Sites may use characters from other languages that look like latin characters, and this makes them look legit. However, they aren't the same as the real website.")
     print(" ")
     print(" ")
-    st = urllib.request.urlopen(w).status
-    if st == 200:
-        print(f'{w} status: up')
-    else:
-        print(f'{w} status: unknown or down')
+    try:
+        st = urllib.request.urlopen(w).status
+        if st == 200:
+            print(f'{w} status: up({st})')
+        else:
+            print(f'{w} status: unknown or down({st})')
+    except:
+        print("Couldn't find the website, or the certificate is incorrect. If you have definitely spelt it correctly, this could be a red flag.")
+        print("One fix is to use http instead of https, if they haven't got an ssl certificate.")
     print(" ")
     print(" ")
